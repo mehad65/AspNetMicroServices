ca0039204e556fa0f5a33a3d3ee7016141965fa9
mehad65 <mehad65@gmail.com>
2022-07-10 10:53:55 +0600

    #main commit

---
+++ Aspnetrun-microservices/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var Db = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products=Db.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogDataSeed.SeedData(Products);
+        }
+
+    }
+}
---
+++ Aspnetrun-microservices/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+    }
+}
